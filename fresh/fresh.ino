dfe0c6ba3934346333d4cd2df383068616446cf3
saar <saarvannetburg@outlook.com>
2026-02-25 19:52:13 +0100

    added temp and light sensor

--- fresh/fresh.ino
+++ fresh/fresh.ino
@@ -1,47 +1,7 @@
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- https://docs.arduino.cc/learn/electronics/lcd-displays
-
-*/
-
 // include the library code:
 #include <LiquidCrystal.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -66,6 +26,12 @@ bool lastPressed = false;
 int sprayCount = 2400;
 int temp = 21;
 
+const uint8_t lightSensor = 10;
+
+#define ONE_WIRE_BUS 8
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature temperature(&oneWire);
+
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -73,6 +39,9 @@ void setup() {
   printMain();
   pinMode(A5, INPUT_PULLUP);
   lastRead = millis();
+
+  pinMode(lightSensor, INPUT);
+  temperature.begin();
 }
 
 void loop() {
@@ -164,13 +133,25 @@ int state(){
   
 }
 
+bool LightOn()
+{
+  int l = analogRead(lightSensor);
+  if (l < 300)
+    return false;
+  return true;
+}
+
 void printMain(){
   
   lcd.setCursor(0, 0);
   lcd.print(sprayCount);
   lcd.print(" sprays   ");
   lcd.setCursor(13, 0);
-  lcd.print(temp);
+
+  temperature.requestTemperatures();
+  float tempC = temperature.getTempCByIndex(0);
+  lcd.print(temp );
+  lcd.print(tempC);
   lcd.print("C");
 
   lcd.setCursor(0,1);
