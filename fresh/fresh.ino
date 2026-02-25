1bf950631b481149c978f2401c85f77d6528d277
Emma <emmajames.real@gmail.com>
2026-02-25 15:07:54 +0100

    Cleaned up

--- fresh/fresh.ino
+++ fresh/fresh.ino
@@ -58,10 +58,10 @@ bool number1 = false;
 bool number2 = false;
 bool cleaning = false;
 
-int currentDelay = 1000;
-int delayShown = 0; //0 - 1000ms, 1 - 10000ms, 2 - 60000ms, 3 - 600000
+//int[] delays = [1000, 10000, 60000, 600000];
+int currentDelay = 0;
+int delayShown = -1; // -1 - current delay, 0 - 1000ms, 1 - 10000ms, 2 - 60000ms, 3 - 600000
 bool poweredOff = false;
-bool lastPressed = false;
 
 int sprayCount = 2400;
 int temp = 21;
@@ -72,53 +72,44 @@ void setup() {
   // Print a message to the LCD.
   printMain();
   pinMode(A5, INPUT_PULLUP);
-  lastRead = millis();
 }
 
 void loop() {
+  //after buttondelay, the value is read off port A5
+  readButtons();
 
-  if(poweredOff){
-    //seizes functionality when powered off
-  }
-  else {
-    //after buttondelay, the value is read off port A5
-    if(millis() - lastRead >= buttonDelay){
-      buttonLadder = analogRead(5);
-      switch (isButton(buttonLadder)) {
-        case 0:
-          //Code for debugging:
-          //lcd.setCursor(0, 0);
-          //lcd.print("last read: ");
-          //lcd.print(lastRead);
-          //lcd.setCursor(0, 1);
-          //lcd.print("currently: ");
-          //lcd.print(millis());
-          break;
-        case 1: 
-          buttonPress1();
-          lastRead = millis();
-          break;
-        case 2:
-          buttonPress2();
-          lastRead = millis();
-          break;
-        case 3:
-          buttonPress3();
-          lastRead = millis();
-          break;
-      }
-      // no button =~ 1005
-      // button 1 =~ 27
-      // button 2 =~ 228
-      // button 3 =~ 358
-      // button 4 (magnetic contact sensor) =~ 449
+}
 
-      // set the cursor to column 0, line 1
-      // (note: line 1 is the second row, since counting begins with 0):
-      //lcd.setCursor(0, 1);
-      // print the read value:
-      //lcd.print(buttonLadder);
+void readButtons(){if(millis() - lastRead >= buttonDelay){
+    buttonLadder = analogRead(5);
+    switch (isButton(buttonLadder)) {
+      case 0:
+        //Code for debugging:
+        //lcd.setCursor(0, 0);
+        //lcd.print("last read: ");
+        //lcd.print(lastRead);
+        //lcd.setCursor(0, 1);
+        //lcd.print("currently: ");
+        //lcd.print(millis());
+        break;
+      case 1: 
+        buttonPress1();
+        lastRead = millis();
+        break;
+      case 2:
+        buttonPress2();
+        lastRead = millis();
+        break;
+      case 3:
+        buttonPress3();
+        lastRead = millis();
+        break;
     }
+    // no button =~ 1005
+    // button 1 =~ 27
+    // button 2 =~ 228
+    // button 3 =~ 358
+    // button 4 (magnetic contact sensor) =~ 449
   }
 }
 
@@ -232,6 +223,7 @@ void buttonPress1() {
       //code that changes the delay based on the displayed delay
       menu = 1;
       printMenuSelector1();
+      break;
   }
 }
 
